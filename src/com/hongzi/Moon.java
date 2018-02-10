db031da8544622b9c799d2d08ede45a38bcd3d78
Estella <estellazhu23@gmail.com>
2018-02-09 19:00:28 -0500

    first commit

---
+++ src/com/hongzi/Moon.java
@@ -0,0 +1,9 @@
+package com.hongzi;
+
+
+public class Moon extends HeavenlyBody {
+
+    public Moon(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.MOON);
+    }
+}
